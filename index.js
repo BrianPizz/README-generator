f424df345c3f068f4e09103970f6c9372b03cf3e
BrianPizz <bpizzimentijr@outlook.com>
2023-09-27 19:06:45 -0400

    added user input questions

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, if any.',
+        name: 'credits'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
