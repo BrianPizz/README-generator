2c0019c3e5b253960c4d3b08d4aa9340ea6da19e
BrianPizz <bpizzimentijr@outlook.com>
2023-09-28 18:08:16 -0400

    added markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -48,11 +48,48 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'none'){
+    return ''
+  } else {
+    return `
+    ## License
+
+    This application is covered under:
+
+    ${renderLicenseLink(license)}
+    `
+  }
+} 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ${renderLicenseBadge(data.license)}
+  # ${data.title}
+
+  ## Description
+  * ${data.motivation}
+  * ${data.reason}
+  * ${data.problem}
+  * ${data.learn}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ${renderLicenseSection(data.license)}
 
 `;
 }
