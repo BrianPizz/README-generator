96dfc8d0a1146db11addfeec1382a4187d45030c
BrianPizz <bpizzimentijr@outlook.com>
2023-09-28 11:52:25 -0400

    updated questions and input type

--- index.js
+++ index.js
@@ -4,29 +4,42 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
+        name: 'title',
         message: 'What is the title of your project?',
-        name: 'title'
     },
     {
-        type: 'input',
-        message: 'Please describe your project.',
-        name: 'description'
+        name: 'motivation',
+        message: 'What was your motivation?',
     },
     {
-        type: 'input',
+        name: 'reason',
+        message: 'Why did you build this project?',
+    },
+    {
+        name: 'problem',
+        message: 'What problem does it solve?',
+    },
+    {
+        name: 'learn',
+        message: 'What did you learn?',
+    },
+    {
+        name: 'install',
         message: 'What are the steps required to install your project?',
-        name: 'install'
     },
     {
-        type: 'input',
+        name: 'usage',
         message: 'Provide instructions and examples for use.',
-        name: 'usage'
     },
     {
-        type: 'input',
+        name: 'credits',
         message: 'List your collaborators, if any.',
-        name: 'credits'
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Choose a license.',
+        choices: ['GNU AGPLv3','GNU GPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense']
     },
 ];
 
@@ -34,7 +47,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
