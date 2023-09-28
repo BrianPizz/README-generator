4f249e5ced523fadbc2d4c06fe668f51a22bf78e
BrianPizz <bpizzimentijr@outlook.com>
2023-09-28 17:46:12 -0400

    added license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license === 'Apache License 2.0'){
-    return '![License](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)'
+    return '![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)'
   } else if(license === 'GNU AGPLv3'){
     return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)'
   } else if (license === 'GNU GPLv3'){
@@ -24,7 +24,27 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'Apache License 2.0'){
+    return '![Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)'
+  } else if(license === 'GNU AGPLv3'){
+    return '![AGPL v3](https://choosealicense.com/licenses/agpl-3.0/)'
+  } else if (license === 'GNU GPLv3'){
+    return '![GPL v3](https://choosealicense.com/licenses/gpl-3.0/)'
+  } else if (license === 'GNU LGPLv3'){
+    return '![LGPL v3](https://choosealicense.com/licenses/lgpl-3.0/)'
+  } else if (license === 'Mozilla Public License 2.0'){
+    return '![MPL 2.0](https://choosealicense.com/licenses/mpl-2.0/)'
+  } else if (license === 'MIT License'){
+    return '![MIT](https://choosealicense.com/licenses/mit/)'
+  } else if (license === 'ISC License (ISC)'){
+    return '![ISC](https://choosealicense.com/licenses/isc/)'
+  } else if (license === 'The Unlicense'){
+    return '![Unlicense](https://choosealicense.com/licenses/unlicense/)'
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
