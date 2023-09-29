cba54dbb93a8471f787daf0319b4bd38220222d4
BrianPizz <bpizzimentijr@outlook.com>
2023-09-28 20:14:42 -0400

    markdown formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,100 +1,101 @@
 // If there is a license, a license badge is returned based on which license is passed in
 function renderLicenseBadge(license) {
   if (license === 'Apache License 2.0') {
-    return '![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)'
+    return '![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)';
   } else if (license === 'GNU AGPLv3') {
-    return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)'
+    return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)';
   } else if (license === 'GNU GPLv3') {
-    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
   } else if (license === 'GNU LGPLv3') {
-    return '![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)'
+    return '![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)';
   } else if (license === 'Mozilla Public License 2.0') {
-    return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-orange.svg)'
+    return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-orange.svg)';
   } else if (license === 'MIT License') {
-    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
   } else if (license === 'ISC License (ISC)') {
-    return '![License: ISC](https://img.shields.io/badge/License-ISC-brightgreen.svg)'
+    return '![License: ISC](https://img.shields.io/badge/License-ISC-brightgreen.svg)';
   } else if (license === 'The Unlicense') {
-    return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-red.svg)'
+    return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-red.svg)';
   } else {
-    return ''
-  }
-}
+    return '';
+  };
+};
 // If there is a license, a license link is returned based on which license is passed in
 function renderLicenseLink(license) {
   if (license === 'Apache License 2.0') {
-    return '[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)'
+    return '[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)';
   } else if (license === 'GNU AGPLv3') {
-    return '[AGPL v3](https://choosealicense.com/licenses/agpl-3.0/)'
+    return '[AGPL v3](https://choosealicense.com/licenses/agpl-3.0/)';
   } else if (license === 'GNU GPLv3') {
-    return '[GPL v3](https://choosealicense.com/licenses/gpl-3.0/)'
+    return '[GPL v3](https://choosealicense.com/licenses/gpl-3.0/)';
   } else if (license === 'GNU LGPLv3') {
-    return '[LGPL v3](https://choosealicense.com/licenses/lgpl-3.0/)'
+    return '[LGPL v3](https://choosealicense.com/licenses/lgpl-3.0/)';
   } else if (license === 'Mozilla Public License 2.0') {
-    return '[MPL 2.0](https://choosealicense.com/licenses/mpl-2.0/)'
+    return '[MPL 2.0](https://choosealicense.com/licenses/mpl-2.0/)';
   } else if (license === 'MIT License') {
-    return '[MIT](https://choosealicense.com/licenses/mit/)'
+    return '[MIT](https://choosealicense.com/licenses/mit/)';
   } else if (license === 'ISC License (ISC)') {
-    return '[ISC](https://choosealicense.com/licenses/isc/)'
+    return '[ISC](https://choosealicense.com/licenses/isc/)';
   } else if (license === 'The Unlicense') {
-    return '[Unlicense](https://choosealicense.com/licenses/unlicense/)'
+    return '[Unlicense](https://choosealicense.com/licenses/unlicense/)';
   } else {
-    return ''
-  }
-}
+    return '';
+  };
+};
 // If there is a license, a license section returned based on which license is passed in
 function renderLicenseSection(license) {
   if (license === 'none') {
-    return ''
+    return '';
   } else {
     return `
 ## License
 This application is covered under:
-${renderLicenseLink(license)}
-  `
-  }
-}
+${renderLicenseLink(license)}`;
+  };
+};
 
 function renderCredits(confirmCredit, credits) {
   if (confirmCredit) {
     return `
 ## Credits
-${credits}`
+${credits}`;
   } else {
-    return ''
-  }
-}
+    return '';
+  };
+};
 
 function renderContribution(confirmContribute) {
   if (confirmContribute) {
     return `## Contribution
-[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)`
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)`;
   } else {
-    return ''
-  }
-}
+    return '';
+  };
+};
 
 function creditsLink(confirm) {
   if (confirm) {
-    return `* [Credits](#credits)`
+    return `* [Credits](#credits)`;
   } else {
     return '';
-  }
-}
+  };
+};
 
 function licenseLink(license) {
   if (license === 'none') {
-    return ''
+    return '';
   } else {
     return `* [License](#license)`;
-  }
-}
+  };
+};
 
 function contributeLink(confirm) {
   if (confirm) {
-    return `* [Contribution](#contribution)`
-  }
-}
+    return `* [Contribution](#contribution)`;
+  } else {
+    return '';
+  };
+};
 
 function generateMarkdown(data) {
   return `
@@ -134,6 +135,6 @@ ${data.test}
 [GitHub](https://github.com/${data.github})  
 [Email](mailto:${data.email})
 `;
-}
+};
 
 module.exports = generateMarkdown;
